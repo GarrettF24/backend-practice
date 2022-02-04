16d66d1c592137efe04bbdcd758f6d6c1001e319
GarrettF <grfoster24@gmail.com>
2022-02-04 14:15:53 -0500

    moved recipes from json file to seed file

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ import logger from "morgan"
 import db from "./db/connection"
 import Recipe from "./models/recipe"
 
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
+// import { createRequire } from "module"
+// const require = createRequire(import.meta.url)
 
-const recipe_file = require("./recipes.json")
+// const recipe_file = require("./recipes.json")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const recipes = recipe_file.recipes
+// const recipes = recipe_file.recipes
 
 app.use(express.json())
 app.use(logger("dev"))
@@ -28,7 +28,6 @@ db.on("connected", () => {
     console.log(`server running on port ${PORT}`)
   })
 })
-
 
 app.get("/", (req, res) => res.send("This is the root"))
 
@@ -42,11 +41,32 @@ app.get("/recipes", async (req, res) => {
   }
 })
 
-
 app.get("/recipes/:id", async (req, res) => {
   try {
     const id = req.params
-    const recipe = await Product.findById(id)
+    const recipe = await Recipe.findById(id)
     res.json(recipe)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message })
   }
 })
+
+app.post("/recipes", async (req, res) => {
+  try {
+    const recipe = new Recipe(req.body)
+    await recipe.save()
+    res.status(201).json(recipe)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.put("/recipes/:id", async (req, res) => {
+  const { id } = req.params
+  const recipe = await Recipe.findByIdAndUpdate(id, req.body, { new: true })
+  res.status(200).json(recipe)
+})
+
+app.delete()
